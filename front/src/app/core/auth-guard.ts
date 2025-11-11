69091e251a2f9b33712433aef662bae8617486ba
Artur trombim <arturtrombim.dev@gmail.com>
2025-11-10 23:34:33 -0300

    feature: adicionar vinculo de token no login e interceptor

--- front/src/app/core/auth-guard.ts
+++ front/src/app/core/auth-guard.ts
@@ -1,14 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth-service';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router)
   const authService = inject(AuthService)
 
   if (authService.getToken()) {
-    return true;
+    return authService.isActive().pipe(
+      map(response => {
+        if (response.status === 200) {
+          return true;
+        } else {
+          router.navigate(['/login']);
+          return false;
+        }
+      }),
+    );
   }
-  
+
   return router.createUrlTree(['/login'])
 };
