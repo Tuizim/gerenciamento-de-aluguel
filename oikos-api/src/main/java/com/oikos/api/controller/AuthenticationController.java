fa66abc9be03cb220e9d6bace027b1941b0558e7
Artur trombim <arturtrombim.dev@gmail.com>
2025-11-10 23:32:22 -0300

    feature: adicionar metodo para verificar se token esta ativo

--- oikos-api/src/main/java/com/oikos/api/controller/AuthenticationController.java
+++ oikos-api/src/main/java/com/oikos/api/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +50,11 @@ public class AuthenticationController {
             );
         }
         userService.createUser(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/isActive")
+    public ResponseEntity isActive(){
         return ResponseEntity.ok().build();
     }
 }
